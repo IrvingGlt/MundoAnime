a672cda53de5aa2a5221f88c4095ca0368316db1
Careaga2000 <careaga.snow@gmail.com>
2021-10-21 23:54:30 -0500

    Actualizacion de los token Alfonso

--- routes/venta.js
+++ routes/venta.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 const Venta = require('../models/venta')
 
-router.get('/', (req, res)=>{
+router.get('/',
+passport.authenticate('jwt', {session:false}),
+(req, res)=>{
     try{
         res.json({
           status:'pagado' ,

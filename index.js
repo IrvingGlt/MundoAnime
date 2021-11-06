6a7f4ccececa61fa15b82cc3f9ef1bef3647ab42
IrvingGlt <irving3567@hotmail.com>
2021-11-06 16:06:02 -0600

    Actualizacion

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { checkApiKey } = require('./middleware/auth.handler')
 const passport = require('passport')
 
 app.use(passport.initialize())
-const port = process.env.PORT || 4000
+const port = config.port
 
 const mongoose = require ('mongoose')
 const USER = encodeURIComponent(config.dbUser)
